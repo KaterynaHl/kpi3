aba94399a37d7f15eca89d1775b0764650b034ba
KaterynaHl <glebakata50@gmail.com>
2026-05-23 07:11:39 +0300

    add read repository for post queries

---
+++ src/application/repositories/PostReadRepository.js
@@ -0,0 +1,11 @@
+class PostReadRepository {
+    findAll(query) {
+      throw new Error("Not implemented");
+    }
+  
+    findById(postId) {
+      throw new Error("Not implemented");
+    }
+  }
+  
+  module.exports = PostReadRepository;
