7b376275af927e12f08fdffd077c1e857ae483df
KaterynaHl <glebakata50@gmail.com>
2026-05-23 09:01:16 +0300

    implement synchronous communication in command handler

--- src/application/command-handlers/CreatePostCommandHandler.js
+++ src/application/command-handlers/CreatePostCommandHandler.js
@@ -1,12 +1,40 @@
-class CreatePostCommandHandler {
-    constructor(postFactory, postRepository) {
+const PostCreatedEvent = require(
+    "../../domain/events/PostCreatedEvent"
+  );
+  
+  class CreatePostCommandHandler {
+    constructor(
+      postFactory,
+      postRepository,
+      notificationService,
+      eventBus
+    ) {
       this.postFactory = postFactory;
       this.postRepository = postRepository;
+      this.notificationService = notificationService;
+      this.eventBus = eventBus;
     }
   
     async handle(command) {
-      const post = this.postFactory.create(command);
+      const post =
+        this.postFactory.create(command);
+  
       await this.postRepository.save(post);
+  
+      // synchronous communication
+      await this.notificationService.send({
+        type: "POST_CREATED",
+        message: `Post ${post.id} created`,
+      });
+  
+      // asynchronous communication
+      await this.eventBus.publish(
+        new PostCreatedEvent({
+          postId: post.id,
+          authorId: post.authorId,
+          content: post.content,
+        })
+      );
   
       return post.id;
     }
