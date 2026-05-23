294adea7f55a418a41efd6bed2b63c0db5741395
KaterynaHl <glebakata50@gmail.com>
2026-05-23 09:14:31 +0300

    add event bus abstraction

---
+++ src/shared/event-bus/EventBus.js
@@ -0,0 +1,11 @@
+class EventBus {
+    subscribe(eventName, handler) {
+      throw new Error("Not implemented");
+    }
+  
+    publish(event) {
+      throw new Error("Not implemented");
+    }
+  }
+  
+  module.exports = EventBus;
