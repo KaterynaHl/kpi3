df563046492e13b2090331b41c1b55a5367d1bda
KaterynaHl <glebakata50@gmail.com>
2026-05-23 08:22:05 +0300

    fix post repository inheritance

--- src/infrastructure/repositories/InMemoryPostRepository.js
+++ src/infrastructure/repositories/InMemoryPostRepository.js
@@ -1,36 +1,49 @@
-const PostReadRepository = require(
-    "../../application/repositories/PostReadRepository"
+const PostRepository = require(
+    "../../domain/repositories/PostRepository"
   );
   
-class InMemoryPostRepository extends PostRepository {
-  constructor(store) {
-    super();
-    this.store = store;
-  }
-
-  async save(post) {
-    const existingIndex = this.store.posts.findIndex((p) => p.id === post.id);
-
-    if (existingIndex === -1) {
-      this.store.posts.push(post);
-    } else {
-      this.store.posts[existingIndex] = post;
+  class InMemoryPostRepository extends PostRepository {
+    constructor(store) {
+      super();
+  
+      this.store = store;
     }
-
-    return post;
+  
+    async save(post) {
+      const existingIndex =
+        this.store.posts.findIndex(
+          (p) => p.id === post.id
+        );
+  
+      if (existingIndex === -1) {
+        this.store.posts.push(post);
+      } else {
+        this.store.posts[existingIndex] =
+          post;
+      }
+  
+      return post;
+    }
+  
+    async findById(id) {
+      return (
+        this.store.posts.find(
+          (post) => post.id === id
+        ) || null
+      );
+    }
+  
+    async findAll() {
+      return this.store.posts;
+    }
+  
+    async delete(id) {
+      this.store.posts =
+        this.store.posts.filter(
+          (post) => post.id !== id
+        );
+    }
   }
-
-  async findById(id) {
-    return this.store.posts.find((post) => post.id === id) || null;
-  }
-
-  async findAll() {
-    return this.store.posts;
-  }
-
-  async delete(id) {
-    this.store.posts = this.store.posts.filter((post) => post.id !== id);
-  }
-}
-
-module.exports = InMemoryPostRepository;
+  
+  module.exports =
+    InMemoryPostRepository;
