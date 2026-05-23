2d1a9c57ef982368140e4aa018b9a8f07063d9f6
KaterynaHl <glebakata50@gmail.com>
2026-05-23 07:48:51 +0300

    fix mistakes

---
+++ src/domain/repositories/UserRepository.js
@@ -0,0 +1,15 @@
+class UserRepository {
+    async save(user) {
+      throw new Error("Not implemented");
+    }
+  
+    async findByEmail(email) {
+      throw new Error("Not implemented");
+    }
+  
+    async findById(id) {
+      throw new Error("Not implemented");
+    }
+  }
+  
+  module.exports = UserRepository;
